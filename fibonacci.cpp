b83d01a217e2a3c5c9343b5b4c72b7e47a8387a3
dadanhdyt <dadanhidyt@gmail.com>
2023-11-10 19:36:10 -1200

    First

--- fibonacci.cpp
+++ fibonacci.cpp
@@ -6,14 +6,14 @@ int main(){
 
     std::cout << "MASUKAN N: ";
     std::cin >> n;
-    std::cout << endl;
+    std::cout << std::endl;
     f_n1 = 0;
     f_n2 = 1;
     f_n = f_n1 + f_n2;
     for ( int i = 1 ; i <= n; i++ )
     {
         f_n = f_n1 + f_n2;
-        f_n2 = f_n2;
+        f_n2 = f_n1;
         f_n1 = f_n;
         std::cout << f_n << std::endl;
     }
